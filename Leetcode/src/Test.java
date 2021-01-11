97044302980b42a2d4a14ae4f2b877cee25c8371
小权 <597141997@qq.com>
2021-01-12 00:21:41 +0800

    update README.md

--- Leetcode/src/Test.java
+++ Leetcode/src/Test.java
@@ -1,3 +1,4 @@
+
 public class Test {
     public static void main(String[] args) {
         System.out.println("12534");
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # algorithm
-**简介**：算法学习、leetcode题编写
+
+**简介**：算法学习、leetcode题编写;
+
+> git 提交代码
+
+ctrl + shift + k
