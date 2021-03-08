b1b0a7374247538fd242d15893ac7b2345637201
andyxu <andyxmq@126.com>
2021-03-08 16:26:43 +0800

    feat: 完成AVLTree

--- data-structure/src/AVLTree.java
+++ data-structure/src/AVLTree.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  *  平衡二叉树
  *  特点: 对于任意节点,左子树和右子树的高度差不能超过1
@@ -33,14 +35,103 @@ public class AVLTree<K extends Comparable<K>, V> {
         return size;
     }
 
+    public boolean isBST(){
+        ArrayList<K> keys = new ArrayList<>();
+        inorder(root, keys);
+        for(int i = 1; i < keys.size(); ++i){
+            if(keys.get(i-1).compareTo(keys.get(i)) > 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean isBalanced(){
+        return isBalanced(root);
+    }
+
+    private boolean isBalanced(Node node){
+        if(node == null){
+            return true;
+        }
+        int balanceFactor = getBalanceFactor(node);
+        if(Math.abs(balanceFactor) > 1){
+            return false;
+        }
+        return isBalanced(node.left) && isBalanced(node.right);
+    }
+
+    private void inorder(Node node, ArrayList<K> keys){
+        if(node == null){
+            return;
+        }
+        inorder(node.left, keys);
+        keys.add(node.key);
+        inorder(node.right, keys);
+    }
+
     public int getHeight(Node node) {
         if(node == null)
             return 0;
         return node.height;
     }
 
+    public int getBalanceFactor(Node node) {
+        if(node == null){
+            return 0;
+        }
+        return getHeight(node.left) - getHeight(node.right);
+    }
+
     public void add(K key, V value){
         root = add(root, key, value);
+    }
+
+    public boolean contains(K key){
+        return getNode(root, key) != null;
+    }
+
+    public void set(K key, V newValue){
+        Node node = getNode(root, key);
+        if(node == null){
+            throw new IllegalArgumentException(key + " doesn't exit");
+        }
+        node.value = newValue;
+    }
+
+    public V get(K key){
+        Node node = getNode(root, key);
+        return node == null ? null : node.value;
+    }
+
+    public V remove(K key){
+        Node node = getNode(root, key);
+        if(node != null){
+            root = remove(root, key);
+            return node.value;
+        }
+        return null;
+    }
+
+    private Node rightRotate(Node y){
+        Node x = y.left;
+        Node T3 = x.right;
+
+        x.right = y;
+        y.left = T3;
+        y.height = 1 + Math.max(getHeight(y.left), getHeight(y.right));
+        x.height = 1 + Math.max(getHeight(x.left), getHeight(x.right));
+        return x;
+    }
+
+    private Node leftRotate(Node y){
+        Node x = y.right;
+        Node T3 = x.left;
+        x.left = y;
+        y.right = T3;
+        y.height = 1 + Math.max(getHeight(y.left), getHeight(y.right));
+        x.height = 1 + Math.max(getHeight(x.left), getHeight(x.right));
+        return x;
     }
 
     private Node add(Node node, K key, V value){
@@ -59,11 +150,124 @@ public class AVLTree<K extends Comparable<K>, V> {
         // 更新height
         node.height = 1 + Math.max(getHeight(node.left), getHeight(node.right));
 
-        // 计算平衡因子
-        int balanceFactor = getHeight(node.left) - getHeight(node.right);
-        if (Math.abs(balanceFactor) > 1) {
-            System.out.println(balanceFactor);
+        // 计算平衡因子维护平衡性
+        int balanceFactor = getBalanceFactor(node);
+        if(balanceFactor > 1 && getBalanceFactor(node.left) >= 0){
+            return rightRotate(node);
+        }
+
+        if(balanceFactor > 1 && getBalanceFactor(node.left) < 0){
+            node.left = leftRotate(node.left);
+            return rightRotate(node);
+        }
+
+        if(balanceFactor < -1 && getBalanceFactor(node.right) <= 0){
+            return leftRotate(node);
+        }
+
+        if(balanceFactor < -1 && getBalanceFactor(node.right) > 0){
+            node.right = rightRotate(node.right);
+            return leftRotate(node);
         }
         return node;
+    }
+
+    private Node getNode(Node node, K key){
+        if(node == null){
+            return null;
+        }
+        if(key.compareTo(node.key) < 0){
+            return getNode(node.left, key);
+        } else if(key.compareTo(node.key) > 0){
+            return getNode(node.right, key);
+        }
+        return node;
+    }
+
+    private Node minimum(Node node){
+        if(node.left == null){
+            return node;
+        }
+        return minimum(node.left);
+    }
+    private Node remove(Node node, K key){
+        if(node == null){
+            return null;
+        }
+
+        Node retNode;
+        if(key.compareTo(node.key) < 0){
+            node.left = remove(node.left, key);
+            retNode = node;
+        } else if(key.compareTo(node.key) > 0){
+            node.right = remove(node.right, key);
+            retNode = node;
+        } else {
+            if(node.left == null){
+                Node rightNode = node.right;
+                node.right = null;
+                size--;
+                retNode = rightNode;
+            } else if(node.right == null){
+                Node leftNode = node.left;
+                node.left = null;
+                size--;
+                retNode = leftNode;
+            } else {
+                Node successor = minimum(node.right);
+                successor.right = remove(node.right, key);
+                successor.left = node.left;
+                node.left = node.right = null;
+                retNode = successor;
+            }
+        }
+
+        if(retNode == null){
+            return null;
+        }
+
+        retNode.height = 1 + Math.max(getHeight(retNode.left), getHeight(retNode.right));
+
+        // 计算平衡因子
+        int balanceFactor = getBalanceFactor(retNode);
+        // 平衡维护
+        if(balanceFactor > 1 && getBalanceFactor(retNode.left) >= 0){
+            return rightRotate(retNode);
+        }
+        if(balanceFactor > 1 && getBalanceFactor(retNode.left) < 0){
+            retNode.left = leftRotate(retNode.left);
+            return rightRotate(retNode);
+        }
+        if(balanceFactor < -1 && getBalanceFactor(retNode.right) <=0){
+            return leftRotate(retNode);
+        }
+        if(balanceFactor < -1 && getBalanceFactor(retNode.right) > 0){
+            retNode.right = rightRotate(retNode.right);
+            return leftRotate(retNode);
+        }
+
+        return retNode;
+    }
+
+
+    public static void main(String[] args) {
+        String filename = "./src/pride-and-prejudice";
+        System.out.println(filename);
+        ArrayList<String> words = new ArrayList<>();
+        if(FileOperation.readFile(filename, words)){
+            System.out.println("Total words:" + words.size());
+            AVLTree<String, Integer> map = new AVLTree();
+
+            for(String word: words){
+                if(map.contains(word)){
+                    map.set(word, map.get(word) + 1);
+                } else {
+                    map.add(word, 1);
+                }
+            }
+            System.out.println(map.isBST());
+            System.out.println(map.isBalanced());
+            System.out.println(map.contains("encoding"));
+        }
     }
 }
