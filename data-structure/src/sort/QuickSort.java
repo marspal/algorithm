38712581dc08cd3624bdaec3b93f9e672cf7459e
andyxu <andyxmq@126.com>
2021-03-15 19:49:32 +0800

    feat: 快速排序

---
+++ data-structure/src/sort/QuickSort.java
@@ -0,0 +1,13 @@
+package sort;
+
+public class QuickSort {
+    private QuickSort(){}
+
+    public static <E extends Comparable<E>> void sort(E[] arr){
+        sort(arr, 0, arr.length - 1);
+    }
+
+    private static <E extends Comparable<E>> void sort(E[] arr, int l, int r){
+
+    }
+}
